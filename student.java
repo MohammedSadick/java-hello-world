d2a7037b313594d6f1bb9da133ebf32ecc474ec0
MohammedSadick <sadickmohammed008@gmail.com>
2026-06-07 07:43:58 +0000

    Add files via upload

---
+++ student.java
@@ -0,0 +1,26 @@
+public class Student {
+    public static void main(String[] args) {
+
+        String fullname = "Mohammed Sadick Abubakar";
+        int age = 21;
+        char gender = 'M';
+        double height = 177.7;
+        double weight = 57.6;
+        boolean studentStatus = true;
+        String phoneNumber = "0539858583";
+        String gradeLevel = "Level 200";
+        String nationality = "Ghanaian";
+
+        System.out.println("PERSONAL INFORMATION");
+        System.out.println("--------------------");
+        System.out.println("FULL NAME: " + fullname);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Height: " + height + " cm");
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Student Status: " + studentStatus);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Grade/Level: " + gradeLevel);
+        System.out.println("Nationality: " + nationality);
+    }
+}
